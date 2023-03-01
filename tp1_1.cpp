0f1f31a10fe1ad1f433e692873c7a381033fbf91
Felix Lamas <felamas@gmail.com>
2023-02-28 21:30:23 -0300

    repaso de punteros

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -2,6 +2,19 @@
 
 int main(int argc, char const *argv[])
 {
-    printf("Hola Mundo");
+    int a, *pt_a;
+
+    a = 100;
+    pt_a = &a;
+    //a) El contenido del puntero
+    printf("El contenido del puntero %d\n", *pt_a);
+    //b) La dirección de memoria almacenada por el puntero.
+    printf("La dirección de memoria almacenada por el puntero %p\n", pt_a);
+    //c) la dirección de memoria de la variable.
+    printf("la dirección de memoria de la variable. %p\n", &a);
+    //d) la dirección de memoria del puntero.
+    printf("la dirección de memoria del puntero. %p\n", &pt_a);
+    //e) el tamaño de memoria utilizado por esa variable usando la función sizeof()
+    printf("tamaño de memoria utiliaza %d", sizeof(a));
     return 0;
 }
